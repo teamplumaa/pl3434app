9bebf4d8e25d696bef2dfff2c530e4b3c8623a15
Baba <babajide.aajayi@gmail.com>
2017-01-03 15:33:13 -0500

    add

--- server/publications.js
+++ server/publications.js
@@ -14,12 +14,52 @@ Meteor.publish('feed', function() {
   return Activities.find({}, {sort: {date: -1}, limit: 10});
 });
 
-Meteor.publish('recipe', function(name) {
-  check(name, String);
+Meteor.publish('plum', function(title) {
+  check(title, String);
   return [
-    BookmarkCounts.find({recipeName: name}),
-    Activities.find({recipeName: name})
+    BookmarkCounts.find({plumName: title}),
+    Activities.find({plumName: title})
   ];
+});
+
+// Meteor.publish('plums', function(){
+//   return Plums.find();
+// });
+
+Meteor.publish('plums', function(){
+  return Plums.find();
+});
+
+Meteor.publish('contests', function(){
+  return Contests.find({}, {sort: {date: -1}, limit: 10});
+});
+
+Meteor.publish('images', function(){
+  return Images.find();
+});
+
+Meteor.publish('users', function(){
+  return Meteor.users.find();
+});
+// Meteor.publish('contest', function(title) {
+//   // check(title, String);
+//   return  [
+//   Contests.find(title),
+//   ContestEntries.find({contestname:title})
+//   ];
+
+//   // return [
+//   //   SubmissionCounts.find({contestName: title}),
+//   //   Activities.find({plumName: name})
+//   // ];
+// });
+
+Meteor.publish('contestentries', function() {
+  return ContestEntries.find();
+});
+
+Meteor.publish('plumComments', function(postId) {
+    return Comments.find({plumId: plumId});
 });
 
 // autopublish the user's bookmarks and admin status
@@ -27,7 +67,7 @@ Meteor.publish(null, function() {
   return Meteor.users.find(this.userId, {
     fields: {
       admin: 1,
-      bookmarkedRecipeNames: 1,
+      bookmarkedPlumNames: 1,
       'services.twitter.profile_image_url_https': 1
     }
   });
